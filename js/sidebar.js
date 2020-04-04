3cf8cd7a0b4bb4880dac6d6c077bc25addff6a70
Massimo Pietracupa <Pietracoops@gmail.com>
2020-04-04 17:12:37 -0400

    Added new pages -Map -Ownship Status -MCL -Rehost Adapter

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,6 +2,10 @@
 $('#SideBar_Home_Page').on('click', function(event) {
 document.getElementById('Home_Page').style.display = "block";
 document.getElementById('Reposition_Page').style.display = "none";
+document.getElementById('Map_Page').style.display = "none";
+document.getElementById('Status_Page').style.display = "none";
+document.getElementById('MCL_Page').style.display = "none";
+document.getElementById('Rehost_Adapter_Page').style.display = "none";
 document.getElementById('Quick_Start_Page').style.display = "none";
 document.getElementById('Freeze_Page').style.display = "none";
 document.getElementById('Resets_Page').style.display = "none";
@@ -10,14 +14,71 @@ document.getElementById('Resets_Page').style.display = "none";
 $('#SideBar_Reposition_Page').on('click', function(event) {
 document.getElementById('Home_Page').style.display = "none";
 document.getElementById('Reposition_Page').style.display = "block";
+document.getElementById('Map_Page').style.display = "none";
+document.getElementById('Status_Page').style.display = "none";
+document.getElementById('MCL_Page').style.display = "none";
+document.getElementById('Rehost_Adapter_Page').style.display = "none";
 document.getElementById('Quick_Start_Page').style.display = "none";
 document.getElementById('Freeze_Page').style.display = "none";
 document.getElementById('Resets_Page').style.display = "none";
 });
 
+$('#SideBar_Map_Page').on('click', function(event) {
+document.getElementById('Home_Page').style.display = "none";
+document.getElementById('Reposition_Page').style.display = "none";
+document.getElementById('Map_Page').style.display = "block";
+document.getElementById('Status_Page').style.display = "none";
+document.getElementById('MCL_Page').style.display = "none";
+document.getElementById('Rehost_Adapter_Page').style.display = "none";
+document.getElementById('Quick_Start_Page').style.display = "none";
+document.getElementById('Freeze_Page').style.display = "none";
+document.getElementById('Resets_Page').style.display = "none";
+});
+
+$('#SideBar_Status_Page').on('click', function(event) {
+document.getElementById('Home_Page').style.display = "none";
+document.getElementById('Reposition_Page').style.display = "none";
+document.getElementById('Map_Page').style.display = "none";
+document.getElementById('Status_Page').style.display = "block";
+document.getElementById('MCL_Page').style.display = "none";
+document.getElementById('Rehost_Adapter_Page').style.display = "none";
+document.getElementById('Quick_Start_Page').style.display = "none";
+document.getElementById('Freeze_Page').style.display = "none";
+document.getElementById('Resets_Page').style.display = "none";
+});
+
+$('#SideBar_MCL_Page').on('click', function(event) {
+document.getElementById('Home_Page').style.display = "none";
+document.getElementById('Reposition_Page').style.display = "none";
+document.getElementById('Map_Page').style.display = "none";
+document.getElementById('Status_Page').style.display = "none";
+document.getElementById('MCL_Page').style.display = "block";
+document.getElementById('Rehost_Adapter_Page').style.display = "none";
+document.getElementById('Quick_Start_Page').style.display = "none";
+document.getElementById('Freeze_Page').style.display = "none";
+document.getElementById('Resets_Page').style.display = "none";
+});
+
+$('#SideBar_Rehost_Adapter_Page').on('click', function(event) {
+document.getElementById('Home_Page').style.display = "none";
+document.getElementById('Reposition_Page').style.display = "none";
+document.getElementById('Map_Page').style.display = "none";
+document.getElementById('Status_Page').style.display = "none";
+document.getElementById('MCL_Page').style.display = "none";
+document.getElementById('Rehost_Adapter_Page').style.display = "block";
+document.getElementById('Quick_Start_Page').style.display = "none";
+document.getElementById('Freeze_Page').style.display = "none";
+document.getElementById('Resets_Page').style.display = "none";
+});
+
+
 $('#SideBar_Quick_Start_Page').on('click', function(event) {
 document.getElementById('Home_Page').style.display = "none";
 document.getElementById('Reposition_Page').style.display = "none";
+document.getElementById('Map_Page').style.display = "none";
+document.getElementById('Status_Page').style.display = "none";
+document.getElementById('MCL_Page').style.display = "none";
+document.getElementById('Rehost_Adapter_Page').style.display = "none";
 document.getElementById('Quick_Start_Page').style.display = "block";
 document.getElementById('Freeze_Page').style.display = "none";
 document.getElementById('Resets_Page').style.display = "none";
@@ -26,6 +87,10 @@ document.getElementById('Resets_Page').style.display = "none";
 $('#SideBar_Freeze_Page').on('click', function(event) {
 document.getElementById('Home_Page').style.display = "none";
 document.getElementById('Reposition_Page').style.display = "none";
+document.getElementById('Map_Page').style.display = "none";
+document.getElementById('Status_Page').style.display = "none";
+document.getElementById('MCL_Page').style.display = "none";
+document.getElementById('Rehost_Adapter_Page').style.display = "none";
 document.getElementById('Quick_Start_Page').style.display = "none";
 document.getElementById('Freeze_Page').style.display = "block";
 document.getElementById('Resets_Page').style.display = "none";
@@ -34,6 +99,10 @@ document.getElementById('Resets_Page').style.display = "none";
 $('#SideBar_Resets_Page').on('click', function(event) {
 document.getElementById('Home_Page').style.display = "none";
 document.getElementById('Reposition_Page').style.display = "none";
+document.getElementById('Map_Page').style.display = "none";
+document.getElementById('Status_Page').style.display = "none";
+document.getElementById('MCL_Page').style.display = "none";
+document.getElementById('Rehost_Adapter_Page').style.display = "none";
 document.getElementById('Quick_Start_Page').style.display = "none";
 document.getElementById('Freeze_Page').style.display = "none";
 document.getElementById('Resets_Page').style.display = "block";
